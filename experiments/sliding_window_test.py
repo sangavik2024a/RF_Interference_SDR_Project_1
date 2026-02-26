5ead444dea4df722b0b0ccf82f072d9dc2a885c8
sangavik2024a <sangavi.k2024a@vitstudent.ac.in>
2026-02-26 21:40:15 +0530

    Add sliding_window_energy function for energy calculation

--- experiments/sliding_window_test.py
+++ experiments/sliding_window_test.py
@@ -1,1 +1,8 @@
+import numpy as np
 
+def sliding_window_energy(x, window_size):
+    energies = []
+    for i in range(len(x) - window_size + 1):
+        window = x[i:i+window_size]
+        energies.append(np.sum(np.abs(window)**2))
+    return np.array(energies)
