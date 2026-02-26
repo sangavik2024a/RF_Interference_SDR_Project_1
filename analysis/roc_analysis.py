fb4008f80d9ce7c354ef21e22b7157cc66b685c4
sangavik2024a <sangavi.k2024a@vitstudent.ac.in>
2026-02-26 21:39:25 +0530

    Add Monte Carlo method for probability detection

--- analysis/roc_analysis.py
+++ analysis/roc_analysis.py
@@ -1,1 +1,16 @@
+import numpy as np
+from detectors.energy_detector import energy_statistic
 
+def monte_carlo_pd(N, sigma, signal_func, noise_func, gamma, trials=10000):
+    detections = 0
+
+    for _ in range(trials):
+        s = signal_func(N)
+        w = noise_func(N, sigma)
+        x = s + w
+
+        T = energy_statistic(x)
+        if T > gamma:
+            detections += 1
+
+    return detections / trials
