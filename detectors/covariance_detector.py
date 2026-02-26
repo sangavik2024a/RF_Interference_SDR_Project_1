53a0a1b2c119e753edf727f2e104ebdd50b859b4
sangavik2024a <sangavi.k2024a@vitstudent.ac.in>
2026-02-26 21:38:57 +0530

    Add covariance_statistic function for first-lag correlation

--- detectors/covariance_detector.py
+++ detectors/covariance_detector.py
@@ -1,1 +1,9 @@
+import numpy as np
 
+def covariance_statistic(x):
+    """
+    Use first-lag correlation magnitude.
+    """
+    N = len(x)
+    R1 = np.sum(x[1:] * np.conj(x[:-1])) / (N - 1)
+    return np.abs(R1)
