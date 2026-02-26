a6a651e9b2388a1a4a6e625a7701613e2e5403dd
sangavik2024a <sangavi.k2024a@vitstudent.ac.in>
2026-02-26 21:37:50 +0530

    Implement complex AWGN sample generation function
    Added a function to generate complex AWGN samples.

--- core/noise_models.py
+++ core/noise_models.py
@@ -1,1 +1,10 @@
+import numpy as np
 
+def generate_complex_awgn(N, sigma):
+    """
+    Generate complex AWGN samples:
+    w[n] ~ CN(0, sigma^2)
+    """
+    noise_real = np.random.normal(0, sigma/np.sqrt(2), N)
+    noise_imag = np.random.normal(0, sigma/np.sqrt(2), N)
+    return noise_real + 1j * noise_imag
