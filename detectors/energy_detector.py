d223e77707a017644189e5c852c7732b08c38650
sangavik2024a <sangavi.k2024a@vitstudent.ac.in>
2026-02-26 21:38:33 +0530

    Add energy statistic and threshold calculation functions

--- detectors/energy_detector.py
+++ detectors/energy_detector.py
@@ -1,1 +1,16 @@
+import numpy as np
+from scipy.stats import norm
 
+def energy_statistic(x):
+    return np.sum(np.abs(x)**2)
+
+def threshold_from_pfa(N, sigma, Pfa):
+    """
+    Gaussian approximation of chi-square statistic
+    """
+    mean = N * sigma**2
+    variance = 2 * N * sigma**4
+    std = np.sqrt(variance)
+
+    gamma = mean + std * norm.ppf(1 - Pfa)
+    return gamma
